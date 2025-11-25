6a5bd72f49bbd64137457a71be32b7cb124117ef
NicholasNPham <phamn1000@gmail.com>
2025-11-24 19:44:47 -0500

    finished video1

--- Decorators/video1.py
+++ Decorators/video1.py
@@ -5,23 +5,30 @@ def dosomething():
     pass
 """
 
+import functools
+
 def start_end_decorator(func):
 
-    def wrapper():
+    @functools.wraps(func) # Help on function add5 in module __main__:
+    def wrapper(*args, **kwargs):
 
         # Something Before
         print("Start")
-        func()
+        result = func(*args, **kwargs)
         # Something After
         print("End")
+        return result
 
     return wrapper
 
 @start_end_decorator
-def print_name():
-    print("Nick")
+# def print_name():
+#     print("Nick")
+def add5(x):
+    return x+5
 
 # This is the same thing as the decorator
 # print_name = start_end_decorator(print_name)
 
-print_name()
+print(help(add5)) # prints wrapper(*args, **kwargs)
+print(add5.__name__) # prints wrapper
