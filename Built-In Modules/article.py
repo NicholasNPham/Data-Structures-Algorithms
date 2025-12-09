23fc045b09f7dea8fc8a5e5227cf996d5db48466
NicholasNPham <phamn1000@gmail.com>
2025-12-08 21:02:57 -0500

    added notes

---
+++ Built-In Modules/article.py
@@ -0,0 +1,17 @@
+"""
+Built-In Modules
+- Function: a separate, complete and reusable software component.
+- Long and complex logic in a program is broken into smaller, independent, and reusable blocks of instructions usually called a module.
+- This approach towards software development is called modular programming.
+
+- Such a program has a main routine through which smaller independent modules (functions) are called upon.
+
+- os module: Functions to perform many tasks of operating system
+- random module: defines various functions for handling randomization
+    - uses pseudo-random generator based upon Mersenne Twister algorithm that produces 53-bit precision floats.
+- math module: mathematical functions
+- sys module : This module provides functions and variables used to manipulate different parts of the python runtime environment
+- collections module: provides alternatives to built-in container data types such as list, tuple and dict
+- statistics module: provides statistical functions
+- time module: time related functions
+"""
